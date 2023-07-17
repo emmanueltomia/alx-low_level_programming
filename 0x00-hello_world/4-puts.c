fb59d361d9cb8f1b4b4690bdc8c6f32e71b5e334
emmanueltomia <emmanueltomia351@gmail.com>
2023-07-17 10:05:44 -0700

    puts.C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * main - A program thats prints a line with puts function
  * return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
