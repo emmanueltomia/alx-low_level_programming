a9e0f9bfee09e24cf5dac926eeaf53f39c918bac
Emmanuel Afubwa <113624821+emmanueltomia@users.noreply.github.com>
2023-07-23 21:33:13 +0300

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,11 +4,12 @@
 * followed by a new line 
 * Return = Always 0 (success)
 */
-int main(void)
-	{
+int main(void) {
+
 	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
+
 	if (ch != 'e' && ch != 'q')
 		putchar(ch);
 	}
