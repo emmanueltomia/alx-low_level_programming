75cd2dcbe88474e0973553e5b85301233216c1ff
Emmanuel Afubwa <113624821+emmanueltomia@users.noreply.github.com>
2023-07-23 21:04:15 +0300

    4-print_alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase expect qand e
+ * followed by a new line 
+ * Return = Always 0 (success)
+ */
+
+int (void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+}
+		
