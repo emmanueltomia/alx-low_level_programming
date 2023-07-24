f59c54cab59c70f3e54eacfda27c20a151fa790c
yobwee-zy <bkingeno07@gmail.com>
2023-07-24 18:20:07 +0300

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* main - prints alphabet in lowercase expect qand e
-* followed by a new line 
+* main - prints alphabet in lowercase expect q and e
+* followed by a new line
 * Return = Always 0 (success)
 */
 int main(void) {
-
 	char ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
